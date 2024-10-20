caae0d23f9c63af22897ab18fddae3d336f3f432
mogaya <mogayainnocent@gmail.com>
2024-10-20 13:57:14 +0300

    6. Populating Your Database with Your More Data

--- task_5.sql
+++ task_5.sql
@@ -2,3 +2,5 @@ USE alx_book_store;
 
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
+
+-- mysql -u your_username -p alx_book_store < task_5.sql
