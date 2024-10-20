f52fd71d0c5d4ff1c90934d39768c91e9d699a5d
mogaya <mogayainnocent@gmail.com>
2024-10-20 07:11:14 +0300

    1. Let's Build Your Database: Your Gateway to Data Adventure! - correction

--- MySQLServer.py
+++ MySQLServer.py
@@ -13,7 +13,7 @@ def create_database():
             mycursor = mydb.cursor()
             mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error connetion to mysql: {e}")
 
     finally:
